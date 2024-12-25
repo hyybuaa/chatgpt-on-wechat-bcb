bc96956ae831bb725ac04abb4e8548b030440ece
huangyuanyuan <huangyuanyuan@baichebao.com>
2024-12-25 20:31:56 +0800

    更新代码

---
+++ test.py
@@ -0,0 +1,31 @@
+import requests
+
+url = "http://1.92.69.23:12003/v1/chat/completions"
+headers = {
+    "Content-Type": "application/json",
+    "API-Key": "sk-bcb_Crcg4k0vAhMkfJft0DOgCImyZ6mEzeOvJEBybZh42HjZdMLkijUhH4ObtQmQM9QE"
+}
+data = {
+    "model": "math-assistant",
+    "messages": [{'role': 'user', 'content': [{'type': 'text', 'text': '根据上下文解读图片内容'}, {'type': 'image_url', 'image_url': '/root/chatgpt-on-wechat-bcb/tmp/241225-202158.jpg'}]}],
+    "stream": False,
+    "user": "user_123"
+}
+# data = {
+#     "model": "math-assistant",
+#     "messages": [
+#         {
+#             "role": "user",
+#             "content": "你好，请帮我解答这个问题。"
+#         }
+#     ],
+#     "stream": False,
+#     "user": "user_123"
+# }
+
+response = requests.post(url, headers=headers, json=data)
+
+print(response.status_code)
+print(response.json())
+
+# 
